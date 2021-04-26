dfb7cfec2294168d1531865cb31511ca3e6ec539
Marco Zanella <marco.zanella.1991@gmail.com>
2021-04-26 09:48:15 +0200

    Added first classes and unit tests

---
+++ tests/CollectorTest.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use \Ittweb\AccelaSearch\ProductMapper\Cms;
+
+final class CmsTest extends TestCase {
+    public function testIdentifier() {
+        $cms = new Cms(1, 'name', 'version');
+        $this->assertEquals(1, $cms->getIdentifier());
+    }
+
+    public function testName() {
+        $cms = new Cms(1, 'name', 'version');
+        $this->assertEquals('name', $cms->getName());
+    }
+
+    public function testVersion() {
+        $cms = new Cms(1, 'name', 'version');
+        $this->assertEquals('version', $cms->getVersion());
+    }
+}
---
+++ tests/ShopTest.php
@@ -0,0 +1,56 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+use \Ittweb\AccelaSearch\ProductMapper\Cms;
+use \Ittweb\AccelaSearch\ProductMapper\Shop;
+
+final class ShopTest extends TestCase {
+    public function testDefaultIsActive() {
+        $shop = $this->create();
+        $this->assertEquals(false, $shop->isActive());
+    }
+
+    public function testIdentifier() {
+        $shop = $this->create()->setIdentifier(1);
+        $this->assertEquals(1, $shop->getIdentifier());
+    }
+
+    public function testUrl() {
+        $data = 'http://www.shop.net';
+        $shop = $this->create()->setUrl($data);
+        $this->assertEquals($data, $shop->getUrl());
+    }
+
+    public function testLanguageIso() {
+        $data = 'fr';
+        $shop = $this->create()->setLanguageIso($data);
+        $this->assertEquals($data, $shop->getLanguageIso());
+    }
+
+    public function testCms() {
+        $data = new Cms(2, 'cms2', '2.0');
+        $shop = $this->create()->setCms($data);
+        $this->assertEquals($data, $shop->getCms());
+    }
+
+    public function testDescription() {
+        $data = 'description';
+        $shop = $this->create()->setDescription($data);
+        $this->assertEquals($data, $shop->getDescription());
+    }
+
+    public function testInitializationTimestamp() {
+        $data = 42;
+        $shop = $this->create()->setInitializationTimestamp($data);
+        $this->assertEquals($data, $shop->getInitializationTimestamp());
+    }
+
+    public function testLastSynchronizationTimestamp() {
+        $data = 42;
+        $shop = $this->create()->setLastSynchronizationTimestamp($data);
+        $this->assertEquals($data, $shop->getLastSynchronizationTimestamp());
+    }
+
+    private function create(): Shop {
+        return new Shop('http://www.shop.com', 'en', new Cms(1, 'cms', '1.0'));
+    }
+}
