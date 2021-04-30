b23c29122a629c9bd4a0da207a13be08762274c8
Marco Zanella <marco.zanella.1991@gmail.com>
2021-04-30 10:02:49 +0200

    Added in memory repositories

---
+++ src/Repository/Memory/Warehouse.php
@@ -0,0 +1,42 @@
+<?php
+namespace Ittweb\AccelaSearch\ProductMapper\Repository\Memory;
+use \OutOfBoundsException;
+use \BadFunctionCallException;
+use \Ittweb\AccelaSearch\ProductMapper\Stock\Warehouse\WarehouseInterface as Subject;
+use \Ittweb\AccelaSearch\ProductMapper\Repository\WarehouseInterface;
+
+class Warehouse {
+    private $warehouses;
+
+    public function __construct() {
+        $this->warehouses = [];
+    }
+
+    public function insert(Subject $warehouse): self {
+        if (is_null($warehouse->getIdentifier())) {
+            throw new BadFunctionCallException('Missing identifier');
+        }
+        $this->warehouses[$warehouse->getIdentifier()] = $warehouse;
+        return $this;
+    }
+
+    public function read(int $identifier): Subject {
+        if (!isset($this->warehouses[$identifier])) {
+            throw new OutOfBoundsException('No warehouse with identifier ' . $identifier);
+        }
+        return $this->warehouses[$identifier];
+    }
+
+    public function update(Subject $warehouse): self {
+        return $this->insert($warehouse);
+    }
+
+    public function delete(Subject $warehouse): self {
+        unset($this->warehouses[$warehouse->getIdentifier()]);
+        return $this;
+    }
+
+    public function search(callable $criterion): array {
+        return array_filter($this->warehouses, $criterion);
+    }
+}
